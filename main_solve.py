ab0c73483489b1973be81e949e9cebf3d5753079
Tasdiqul <tasdiq088@gmail.com>
2022-12-28 13:39:31 -0800

    new nodes

--- main_solve.py
+++ main_solve.py
@@ -7,9 +7,9 @@ import time
 def solve(G , L_max , delta):
     center_nodes = compute_center_nodes(G , L_max, delta)
     print("len of c " , len(center_nodes))
-    # add_quantum_repeater_between_centers(G , center_nodes , L_max * .9   )
-    # compute_shortest_path(G)
-    # check_solution(G , center_nodes , L_max)
+    add_quantum_repeater_between_centers(G , center_nodes , L_max * .9   )
+    compute_shortest_path(G)
+    check_solution(G , center_nodes , L_max)
 
     # critical_paths = compute_shortest_path_between_centers(G , center_nodes , L_max)
 
