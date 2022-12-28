edb4ddf45c54cae5fbbca8f8e10ea8da4f9e05b0
Tasdiqul <tasdiq088@gmail.com>
2022-12-28 11:33:17 -0800

    new nodes

--- main_solve.py
+++ main_solve.py
@@ -9,7 +9,7 @@ def solve(G , L_max , delta):
     print("len of c " , len(center_nodes))
     add_quantum_repeater_between_centers(G , center_nodes , L_max * .9   )
     compute_shortest_path(G)
-    check_solution(G , center_nodes , L_max)
+    # check_solution(G , center_nodes , L_max)
 
     # critical_paths = compute_shortest_path_between_centers(G , center_nodes , L_max)
 
@@ -27,7 +27,7 @@ def main(file_name):
     solve(G , L_max , delta)
     print("calculating output")
 
-    calculate_output(G)
+    calculate_output()
     
 
 
