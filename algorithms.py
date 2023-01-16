674cf074bde38e5a49e42db6150046a0d1a92a12
Tasdiqul Islam <tasdiqulislam@Tasdiquls-MBP.lan>
2023-01-16 11:01:29 -0800

    check pairs with k

--- algorithms.py
+++ algorithms.py
@@ -218,20 +218,20 @@ def is_feasible_path(path , center_nodes , L_max):
     
     return True
 
-def check_pairs(G, center_nodes , L_max , unique_end_node_pairs , thread_no , k ):
-    if k ==1:
-        check_pairs_k(G2, center_nodes , L_max , unique_end_node_pairs , thread_no )
-    else:
-        random_down_nodes_ = list(itertools.combinations(center_nodes, r=k-1))
-        for tup in random_down_nodes_:
-            G2 = G.copy()
-            for i in range(k-1):
-                G2.remove_node(tup[i])
-            check_pairs_k(G2, center_nodes , L_max , unique_end_node_pairs , thread_no )
+# def check_pairs_2(G, center_nodes , L_max , unique_end_node_pairs , thread_no , k ):
+#     if k ==1:
+#         check_pairs_k(G2, center_nodes , L_max , unique_end_node_pairs , thread_no )
+#     else:
+#         random_down_nodes_ = list(itertools.combinations(center_nodes, r=k-1))
+#         for tup in random_down_nodes_:
+#             G2 = G.copy()
+#             for i in range(k-1):
+#                 G2.remove_node(tup[i])
+#             check_pairs_k(G2, center_nodes , L_max , unique_end_node_pairs , thread_no )
 
             
             
-def check_pairs_k(G, center_nodes , L_max , unique_end_node_pairs , thread_no ):
+def check_pairs(G, center_nodes , L_max , unique_end_node_pairs , thread_no ):
 
     solution_exists = True
     pair_no = 0
